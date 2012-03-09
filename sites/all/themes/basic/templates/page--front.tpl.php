d2e65f243854acdae3fdc306899eeb96339d8e93
Roberto <rmartin.martin@gmail.com>
2012-03-08 23:16:29 -0500

    adding style and tagadelic module

---
+++ sites/all/themes/basic/templates/page--front.tpl.php
@@ -0,0 +1,129 @@
+
+<div class="container header" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <div class="row topheader">
+        <div class="eight columns">
+          <?php $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL)); ?>
+          <?php $logo_path = $theme_path . "/images/focuslogo.png"; ?>
+          <a class="logo" rel="home" href="<?php print $front_page; ?>"><img src="<?php print $logo_path; ?>"/></a>
+          
+        </div><!-- /eight columns -->
+        <div class="four columns">
+           <?php if ($page['header']): ?>
+             <?php print render($page['header']); ?>
+           <?php endif; ?>
+          <ul class="social">
+            <li><a href="#"><img src="<?php echo $theme_path ?>/images/facebook.jpg"></a></li>
+            <li><a href="#"><img src="<?php echo $theme_path ?>/images/twitter.jpg"></a></li>
+            <li><a href="#"><img src="<?php echo $theme_path ?>/images/youtube.jpg"></a></li>
+            <li><a href="#"><img src="<?php echo $theme_path ?>/images/feed.jpg"></a></li>
+          </ul>
+ 
+        </div><!-- /four columns -->
+      </div><!-- /row -->
+      <div class="row navrow">
+        <div class="eight columns offset-by-four">
+          <!--<ul class="navbar">-->
+            <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu, navbar')))); ?>
+            <!--<li><a href="#">Home</a></li>
+            
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Q&A</a></li>
+            <li><a href="#">Workout Blog</a></li>
+            <li><a href="#">Contact</a></li>-->
+          <!--</ul>-->
+        </div>
+        
+      </div><!-- /navbar -->
+  
+  
+</div><!-- /ends header -->
+
+
+<div class="container main-content">
+      <div class="row">
+        <div class="seven columns content">
+          
+          <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
+          <div id="content-header">
+
+              <?php print $breadcrumb; ?>
+
+              <?php if ($page['highlight']): ?>
+                <div id="highlight"><?php print render($page['highlight']) ?></div>
+              <?php endif; ?>
+
+              <?php if ($title): ?>
+                <h1 class="title"><?php print $title; ?></h1>
+              <?php endif; ?>
+
+              <?php print $messages; ?>
+              <?php print render($page['help']); ?>
+
+              <?php if ($tabs): ?>
+                <div class="tabs"><?php print render($tabs); ?></div>
+              <?php endif; ?>
+
+              <?php if ($action_links): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+              <?php endif; ?>
+
+            </div> <!-- /#content-header -->
+          <?php endif; ?>
+            
+          <div id="content-area">
+             <?php print render($page['content']) ?>
+          </div>
+
+          
+          <h1 class="welcome">Welcome to Focused Fitness NYC</h1>
+          <p>With Focused Fitness NYC the pain you gain is the Pain of Wellness
+When you become our client you have taken the first positive step in choosing a LIFESTYLE OF WELLNESS.  That lifestyle DOES NOT require you to become a gym rat or a member of the food police.  It does mean making some changes though that will help you improve your life all around. </p>
+          
+          <hr/>
+          
+        </div>
+        
+        
+        
+        <div class="five columns side-content">
+          <div class="about">
+            <h2>About Me</h2>
+            <img class="profile-pic" src="<?php echo $theme_path ?>/images/photo.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nunc id magna euismod eu vestibulum diam interdum.</p>
+            <p>Phasellus nec libero ut arcu consectetur tempus. Etiam luctus congue velit, sed venenatis risus tempus vitae. Etiam quis felis metus, non faucibus lacus.</p>
+            <h2>Get in Touch</h2>
+            <p>contact form</p>
+          </div><!-- /about -->
+          
+          <?php print render($page['sidebar_second']); ?>
+          
+        </div>
+        
+      </div>
+      
+    </div><!-- /main-content -->
+    
+    <div class="containerfooter">
+      <div class="row">
+        <div class="twelve columns">
+          
+          
+              
+              <?php print render($page['footer']); ?>
+              <div>&COPY;Focused Fitness NYC, 2012</div>
+          
+        </div>
+      </div>
+            
+
+    </div>
+
+
+
+
+
+
+
+
+
---
+++ sites/all/themes/basic/templates/page.tpl.php
@@ -0,0 +1,126 @@
+
+<div class="container header" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+
+  <div class="row topheader">
+        <div class="eight columns">
+          <?php $theme_path = drupal_get_path('theme', variable_get('theme_default', NULL)); ?>
+          <?php $logo_path = $theme_path . "/images/focuslogo.png"; ?>
+          <a class="logo" rel="home" href="<?php print $front_page; ?>"><img src="/<?php print $logo_path; ?>"/></a>
+          
+        </div><!-- /eight columns -->
+        <div class="four columns">
+           <?php if ($page['header']): ?>
+             <?php print render($page['header']); ?>
+           <?php endif; ?>
+          <ul class="social">
+            <li><a href="#"><img src="/<?php echo $theme_path ?>/images/facebook.jpg"></a></li>
+            <li><a href="#"><img src="/<?php echo $theme_path ?>/images/twitter.jpg"></a></li>
+            <li><a href="#"><img src="/<?php echo $theme_path ?>/images/youtube.jpg"></a></li>
+            <li><a href="#"><img src="/<?php echo $theme_path ?>/images/feed.jpg"></a></li>
+          </ul>
+ 
+        </div><!-- /four columns -->
+      </div><!-- /row -->
+      <div class="row navrow">
+        <div class="eight columns offset-by-four">
+          <!--<ul class="navbar">-->
+            <?php print theme('links', array('links' => $main_menu, 'attributes' => array('id' => 'primary', 'class' => array('links', 'clearfix', 'main-menu, navbar')))); ?>
+            <!--<li><a href="#">Home</a></li>
+            
+            <li><a href="#">Programs</a></li>
+            <li><a href="#">Q&A</a></li>
+            <li><a href="#">Workout Blog</a></li>
+            <li><a href="#">Contact</a></li>-->
+          <!--</ul>-->
+        </div>
+        
+      </div><!-- /navbar -->
+  
+  
+</div><!-- /ends header -->
+
+
+<div class="container main-content">
+      <div class="row">
+        <div class="eight columns content">
+          
+          <?php if ($breadcrumb || $title|| $messages || $tabs || $action_links): ?>
+          <div id="content-header">
+
+              <?php print $breadcrumb; ?>
+
+              <?php if ($page['highlight']): ?>
+                <div id="highlight"><?php print render($page['highlight']) ?></div>
+              <?php endif; ?>
+
+              <?php if ($title): ?>
+                <h1 class="title"><?php print $title; ?></h1>
+              <?php endif; ?>
+
+              <?php print $messages; ?>
+              <?php print render($page['help']); ?>
+
+              <?php if ($tabs): ?>
+                <div class="tabs"><?php print render($tabs); ?></div>
+              <?php endif; ?>
+
+              <?php if ($action_links): ?>
+                <ul class="action-links"><?php print render($action_links); ?></ul>
+              <?php endif; ?>
+
+            </div> <!-- /#content-header -->
+          <?php endif; ?>
+            
+          <div id="content-area">
+             <?php print render($page['content']) ?>
+          </div>
+
+          
+          
+          
+        </div>
+        
+        
+        
+        <div class="four columns side-content">
+          <div class="about">
+            <h2>About Me</h2>
+            <img class="profile-pic" src="/<?php echo $theme_path ?>/images/photo.jpg">
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod nunc id magna euismod eu vestibulum diam interdum.</p>
+            <p>Phasellus nec libero ut arcu consectetur tempus. Etiam luctus congue velit, sed venenatis risus tempus vitae. Etiam quis felis metus, non faucibus lacus.</p>
+            <h2>Get in Touch</h2>
+            <p>contact form</p>
+          </div><!-- /about -->
+          
+          <?php print render($page['sidebar_second']); ?>
+          
+        </div>
+        
+      </div>
+      
+    </div><!-- /main-content -->
+    
+    <div class="containerfooter">
+      <div class="row">
+        <div class="twelve columns">
+          
+          <?php print render($page['footer']); ?>
+          <div>&COPY;Focused Fitness NYC</div>
+            
+            
+          
+        </div>
+      </div>
+            
+
+    </div>
+
+
+
+
+
+
+
+
+
+
